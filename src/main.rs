5ad44c6e5a0addc787ddaf13b36ef4ecb7adf98f
fumiya-kume <fumiya.kume@hotmail.com>
2023-01-08 19:40:04 +0900

    Replace to 数当て

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+
+
 
 fn main() {
-    let stdout = stdout();
-    let message = "Hello fellow Rustaceans!";
-    let width = message.chars().count();
+    let mut input = String::new();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message, width, &mut writer).unwrap();
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read");
+    println!("You input: {}", input);
 }
