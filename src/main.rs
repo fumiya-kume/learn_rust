b896408a1c9a02e7c430b35ffaa016057eaab0e4
fumiya-kume <fumiya.kume@hotmail.com>
2023-01-08 19:41:15 +0900

    Fix code format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-
-
+use std::io;
 
 fn main() {
     let mut input = String::new();
 
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read");
+    io::stdin().read_line(&mut input).expect("Failed to read");
     println!("You input: {}", input);
 }
